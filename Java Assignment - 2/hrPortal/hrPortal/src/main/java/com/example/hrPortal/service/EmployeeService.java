3c86e156e72d0508dcdc0b8180e3a8bf9549ca18
Ishika Sadhwani <111297284+ishikasadhwani@users.noreply.github.com>
2026-05-06 11:21:40 +0530

    Fix formatting and ensure proper package declaration

--- Java Assignment - 2/hrPortal/hrPortal/src/main/java/com/example/hrPortal/service/EmployeeService.java
+++ Java Assignment - 2/hrPortal/hrPortal/src/main/java/com/example/hrPortal/service/EmployeeService.java
@@ -5,7 +5,7 @@ import com.example.hrPortal.repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.*
 
 @Service
 public class EmployeeService {
